943555f255dfe4ef3756a4a7128b20875786b171
최영재 <brandon94_@naver.com>
2021-04-02 03:39:03 +0900

    baekjoon 2309 일곱 난쟁이

--- baekjoon/baekjoon 1766 문제집/1766.py
+++ baekjoon/baekjoon 1766 문제집/1766.py
@@ -7,20 +7,19 @@ heap, result = [], []
 
 for _ in range(m):
     a, b = map(int, input().split())
-    problem[a].append(b)
-    pre[b] += 1
+    problem[a].append(b)  # 연결된 간선. a번 문제는 b번보다 먼저 풀어야한다
+    pre[b] += 1  # b번문제는 몇개의 선행문제와 연결되어있는지를 카운팅한다.
 
 for i in range(1, n+1):
-    if pre[i] == 0:
+    if pre[i] == 0:  # 아무런 선행문제가 없는 경우
         heapq.heappush(heap, i)
 
-
 while heap:
-    num = heapq.heappop(heap)
+    num = heapq.heappop(heap)  # 선행문제가 없는 문제들 중 난이도가 낮은 것부터 꺼낸다
     result.append(num)
-    for i in problem[num]:
+    for i in problem[num]:  # 해당 선행문제를 풀었을 때 풀 수 있는 문제들을 확인한다.
         pre[i] -= 1
-        if pre[i] == 0:
+        if pre[i] == 0:  # 더 이상 선행문제가 없으면 0이되고, 그러면 그 문제를 heap에 넣는다.
             heapq.heappush(heap, i)
 
 for i in result:
---
+++ baekjoon/baekjoon 2309 일곱 난쟁이/2309.py
@@ -0,0 +1,24 @@
+import sys
+
+dwarf = []
+for _ in range(9):
+    dwarf.append(int(sys.stdin.readline()))
+
+
+sum = sum(dwarf)
+# while(len(dwarf) == 9):
+for i in range(9):
+    for q in range(i+1, 9):
+        if (sum-(dwarf[i]+dwarf[q]) == 100):
+            fake1 = dwarf[i]
+            fake2 = dwarf[q]
+            dwarf.remove(fake1)
+            dwarf.remove(fake2)
+            break
+    if(len(dwarf) != 9):
+        break
+
+
+dwarf.sort()
+for i in dwarf:
+    print(i)
