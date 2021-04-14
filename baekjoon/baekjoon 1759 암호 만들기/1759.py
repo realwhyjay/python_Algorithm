830260c19fe54c31e7abfe5a7a1cad49ef3bb5b1
최영재 <brandon94_@naver.com>
2021-04-14 14:04:27 +0900

    1759 ing

--- baekjoon/baekjoon 1759 암호 만들기/1759.py
+++ baekjoon/baekjoon 1759 암호 만들기/1759.py
@@ -1,9 +1,21 @@
 from itertools import combinations
 
-
 l, c = map(int, input().split())
 character = list(input().split())
 pw = list(combinations(sorted(character), 4))
+vowel = ['a', 'e', 'o', 'i', 'u']
+result = []
+mo, ja = 0, 0
+pw.sort()
+
+for i in pw:
+    for j in i:
+        if j in vowel:
+            mo += 1
+        else:
+            ja += 1
+    if mo >= 1 and ja >= 2:
+        result.append(i)
 
 for i in pw:
     for j in i:
