1288fe230696034e9faa55797be107ba03ff388a
realwhyjay <brandon94_@naver.com>
2021-08-29 02:15:08 +0900

    baekjoon 1929

--- baekjoon/baekjoon 1929 소수 구하기/1929 2트.py
+++ baekjoon/baekjoon 1929 소수 구하기/1929 2트.py
@@ -1,16 +1,12 @@
 M, N = map(int, input().split())
 
-check = [False, False] + [True]*(N-1)
+check = [True]*(N+1)
 
-for i in range(2, int(N**0.5)+1):
-    print("i : ", i)
+for i in range(2, int((N+1)**0.5)+1):
     if check[i] == True:
-        for j in range(i+i, N+1, i):
+        for j in range(2*i, N+1, i):
             check[j] = False
-            # print(check)
 
 for k in range(M, N+1):
-    if check[k] == True:
+    if k > 1 and check[k] == True:
         print(k)
-
-print("check : ", check)
